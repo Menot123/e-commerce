7290a47cc104ac76777c95e584ad984acc5092d7
Menot123 <ngjyentiendat9a3@gmail.com>
2024-10-18 14:10:57 +0700

    Show product from fake api

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,51 +1,100 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Product.css';
+import axios from "axios";
 import { Card, Col, Row, Button, Input, Pagination } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 
+type Category = {
+    id: number;
+    name: string;
+    image: string;
+    creationAt: Date;
+    updatedAt: Date;
+};
 
-const products = [
-    { "id": 1, "name": "Sản phẩm 1", "description": "Đây là sản phẩm 1", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 2, "name": "Sản phẩm 2", "description": "Đây là sản phẩm 2", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 3, "name": "Sản phẩm 3", "description": "Đây là sản phẩm 3 Đây là sản phẩm 3", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 4, "name": "Sản phẩm 4", "description": "Đây là sản phẩm 4", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 5, "name": "Sản phẩm 5", "description": "Đây là sản phẩm 5", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 6, "name": "Sản phẩm 6", "description": "Đây là sản phẩm 6", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 7, "name": "Sản phẩm 7", "description": "Đây là sản phẩm 7", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 8, "name": "Sản phẩm 8", "description": "Đây là sản phẩm 8", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 9, "name": "Sản phẩm 9", "description": "Đây là sản phẩm 9", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 10, "name": "Sản phẩm 10", "description": "Đây là sản phẩm 10.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 11, "name": "Sản phẩm 11", "description": "Đây là sản phẩm 11.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 12, "name": "Sản phẩm 12", "description": "Đây là sản phẩm 12.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 13, "name": "Sản phẩm 13", "description": "Đây là sản phẩm 13.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 14, "name": "Sản phẩm 14", "description": "Đây là sản phẩm 14.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 15, "name": "Sản phẩm 15", "description": "Đây là sản phẩm 15.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 16, "name": "Sản phẩm 16", "description": "Đây là sản phẩm 16.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 17, "name": "Sản phẩm 17", "description": "Đây là sản phẩm 17.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 18, "name": "Sản phẩm 18", "description": "Đây là sản phẩm 18.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 19, "name": "Sản phẩm 19", "description": "Đây là sản phẩm 19.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 20, "name": "Sản phẩm 20", "description": "Đây là sản phẩm 20.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 21, "name": "Sản phẩm 21", "description": "Đây là sản phẩm 21.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 22, "name": "Sản phẩm 22", "description": "Đây là sản phẩm 22.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 23, "name": "Sản phẩm 23", "description": "Đây là sản phẩm 23.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 24, "name": "Sản phẩm 24", "description": "Đây là sản phẩm 24.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
-    { "id": 25, "name": "Sản phẩm 25", "description": "Đây là sản phẩm 25.", "price": 100, "imageUrl": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' }
-]
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string[];
+    creationAt: Date;
+    updatedAt: Date;
+    category: Category[];
+};
+
+
+// const products = [
+//     { "id": 1, "title": "Sản phẩm 1", "description": "Đây là sản phẩm 1", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 2, "title": "Sản phẩm 2", "description": "Đây là sản phẩm 2", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 3, "title": "Sản phẩm 3", "description": "Đây là sản phẩm 3 Đây là sản phẩm 3", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 4, "title": "Sản phẩm 4", "description": "Đây là sản phẩm 4", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 5, "title": "Sản phẩm 5", "description": "Đây là sản phẩm 5", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 6, "title": "Sản phẩm 6", "description": "Đây là sản phẩm 6", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 7, "title": "Sản phẩm 7", "description": "Đây là sản phẩm 7", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 8, "title": "Sản phẩm 8", "description": "Đây là sản phẩm 8", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 9, "title": "Sản phẩm 9", "description": "Đây là sản phẩm 9", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 10, "title": "Sản phẩm 10", "description": "Đây là sản phẩm 10.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 11, "title": "Sản phẩm 11", "description": "Đây là sản phẩm 11.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 12, "title": "Sản phẩm 12", "description": "Đây là sản phẩm 12.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 13, "title": "Sản phẩm 13", "description": "Đây là sản phẩm 13.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 14, "title": "Sản phẩm 14", "description": "Đây là sản phẩm 14.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 15, "title": "Sản phẩm 15", "description": "Đây là sản phẩm 15.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 16, "title": "Sản phẩm 16", "description": "Đây là sản phẩm 16.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 17, "title": "Sản phẩm 17", "description": "Đây là sản phẩm 17.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 18, "title": "Sản phẩm 18", "description": "Đây là sản phẩm 18.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 19, "title": "Sản phẩm 19", "description": "Đây là sản phẩm 19.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 20, "title": "Sản phẩm 20", "description": "Đây là sản phẩm 20.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 21, "title": "Sản phẩm 21", "description": "Đây là sản phẩm 21.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 22, "title": "Sản phẩm 22", "description": "Đây là sản phẩm 22.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 23, "title": "Sản phẩm 23", "description": "Đây là sản phẩm 23.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 24, "title": "Sản phẩm 24", "description": "Đây là sản phẩm 24.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' },
+//     { "id": 25, "title": "Sản phẩm 25", "description": "Đây là sản phẩm 25.", "price": 100, "images": 'https://m.media-amazon.com/images/I/719baS3kW5L._AC_SX569_.jpg' }
+// ]
 
 const Product: React.FC = () => {
-    // return (
 
-    //     // <div className='product-container'>
-    //     //     {/* <img src={loginBackground} alt="Sản phẩm Background" /> */}
-    //     // </div>
-    // );
+    // const productsAPI: string = import.meta.env.VITE_PRODUCT_API as string;
+
+    const getFirstImageUrl = (images: string[]): string => {
+        if (images.length == 1) return images[0].slice(2, -2);
+        if (images.length > 1) return images[0]
+        else return 'none';
+
+    };
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
     const [searchTerm, setSearchTerm] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
     const pageSize = 18; // Số lượng sản phẩm trên mỗi trang
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get<Product[]>('https://api.escuelajs.co/api/v1/products');
+                setProducts(response.data);
+            } catch (err) {
+                setError('Có lỗi xảy ra khi tải dữ liệu');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+    if (loading) {
+        return <div>Đang tải...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
     const filteredProducts = products.filter((product) =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const paginatedProducts = filteredProducts.slice(
@@ -80,7 +129,7 @@ const Product: React.FC = () => {
                     <Col xs={24} sm={12} md={6} lg={4} key={product.id}>
                         <Card
                             hoverable
-                            cover={<img alt={product.name} src={product.imageUrl} style={{ height: '200px', objectFit: 'cover' }} />}
+                            cover={<img alt={product.title} src={getFirstImageUrl(product.images)} style={{ height: '200px', objectFit: 'cover' }} />}
                             style={{ fontSize: '14px', padding: '10px' }}
                             actions={[
                                 <Button
@@ -93,12 +142,12 @@ const Product: React.FC = () => {
                             ]}
                         >
                             <Card.Meta
-                                title={product.name}
+                                title={product.title}
                                 description={
                                     <>
-                                        <p style={{ margin: '0px', height: '50px' }}>{product.description}</p>
+                                        <p className='ellipsis' style={{ margin: '0px', height: '50px' }}>{product.description}</p>
                                         <p style={{ margin: '0px', fontWeight: 'bold', color: '#1890ff' }}>
-                                            {product.price.toFixed(0)} VNĐ
+                                            ${product.price.toFixed(0)}
                                         </p>
                                     </>
                                 }
